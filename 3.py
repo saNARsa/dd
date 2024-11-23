914630361008aa577407507b1df674577bc84562
saNARsa <rockking5336@gmail.com>
2024-11-24 00:06:47 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,12 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a , b , c)
+print_params()
+print_params( b=25 )
+print_params(c = [1,2,3])
+values_list = [12, 'prin' , True]
+values_dict = {'a' : 1 , 'b' : 2 , 'c' : 3}
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [54.32, 'Строка' ]
+
+print_params(*values_list_2, 42)
